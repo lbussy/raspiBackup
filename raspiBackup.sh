5d02aae79bbac943bc3adc6986b12229506f26b3
framp <framp@linux-tips-and-tricks.de>
2020-04-08 22:56:16 +0200

    Sync with local

--- raspiBackup.sh
+++ raspiBackup.sh
@@ -61,11 +61,11 @@ IS_HOTFIX=$(( ! $(grep -iq hotfix <<< "$VERSION"; echo $?) ))
 MYSELF=${0##*/}
 MYNAME=${MYSELF%.*}
 
-GIT_DATE="$Date: 2020-04-07 20:48:34 +0200$"
+GIT_DATE="$Date: 2020-04-08 21:23:22 +0200$"
 GIT_DATE_ONLY=${GIT_DATE/: /}
 GIT_DATE_ONLY=$(cut -f 2 -d ' ' <<< $GIT_DATE)
 GIT_TIME_ONLY=$(cut -f 3 -d ' ' <<< $GIT_DATE)
-GIT_COMMIT="$Sha1: 5a6e009$"
+GIT_COMMIT="$Sha1: 37da3bf$"
 GIT_COMMIT_ONLY=$(cut -f 2 -d ' ' <<< $GIT_COMMIT | sed 's/\$//')
 
 GIT_CODEVERSION="$MYSELF $VERSION, $GIT_DATE_ONLY/$GIT_TIME_ONLY - $GIT_COMMIT_ONLY"
